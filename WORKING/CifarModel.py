98c4433e732efb9cbd62202673073941482218e0
AK1R4S4T0H <deavers.chadwick@gmail.com>
2023-05-16 23:15:39 -0400

    added CifarModel

---
+++ WORKING/CifarModel.py
@@ -0,0 +1,46 @@
+# uses Cifar00 dataset for training
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from keras.datasets import cifar100
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+# Load and preprocess the CIFAR-100 dataset
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=100)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=100)
+
+# model 
+model = Sequential()
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(100, activation='softmax'))
+
+# Compile
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train
+history = model.fit(x_train, y_train, batch_size=64, epochs=100, validation_data=(x_test, y_test))
+
+# Save
+model.save("model.h5")
+print("Model saved successfully!")
+
+# training and validation accuracy
+plt.figure(figsize=(8, 6))
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
