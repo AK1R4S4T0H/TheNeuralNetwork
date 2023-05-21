f7d3205681c1a492beb54b0c61014c92708574bb
AK1R4S4T0H <deavers.chadwick@gmail.com>
2023-05-20 20:19:59 -0400

    update

--- WORKING/FITW_Model.py
+++ WORKING/FITW_Model.py
@@ -11,19 +11,19 @@ lfw_dataset = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
 x_data = lfw_dataset.images
 y_data = lfw_dataset.target
 
-# Filter dataset
+# Filter
 face_indices = np.where(np.isin(y_data, [0, 1]))[0]
 x_data = x_data[face_indices]
 y_data = y_data[face_indices]
 
-# Split dataset
+# Split
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=42)
 
 # Normalize pixel values to [0, 1]
 x_train = x_train / 255.0
 x_test = x_test / 255.0
 
-# one-hot encoded vectors
+# one-hot encode vectors
 y_train = tf.keras.utils.to_categorical(y_train, num_classes=2)
 y_test = tf.keras.utils.to_categorical(y_test, num_classes=2)
 
@@ -36,17 +36,13 @@ model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(2, activation='softmax'))
 
-# Compile
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train
 history = model.fit(x_train, y_train, batch_size=32, epochs=100, validation_data=(x_test, y_test))
 
-# Save
 model.save("FITW.h5")
-print("Model saved successfully!")
+print("Model saved")
 
-# plot
 plt.figure(figsize=(8, 6))
 plt.plot(history.history['accuracy'], label='Training Accuracy')
 plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
